f022d2dea6877b0b113e0ecbf1e209e4e7e4b1c6
Cory Tveten <corytveten@gmail.com>
2022-01-03 22:04:03 -0500

    display function

--- calculator/app.js
+++ calculator/app.js
@@ -1,1 +1,19 @@
-console.log('load')
+window.addEventListener('DOMContentLoaded', () => {
+    console.log("load")
+    displayNumber()
+})
+
+function displayNumber() {
+    const display = document.getElementById('display');
+    const numberGrid = document.getElementById('number-grid');
+
+    numberGrid.addEventListener('click', (e) => {
+        console.log(e.target.innerText)
+        if (display.innerText === '0') {
+            display.innerText = e.target.innerText
+        } else {
+            display.innerText += e.target.innerText
+        }
+    })
+    
+}
