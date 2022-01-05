850cc9235c45867d564799f7d6e067e612dbd496
Cory Tveten <corytveten@gmail.com>
2022-01-04 21:53:20 -0500

    update functions

--- calculator/app.js
+++ calculator/app.js
@@ -1,19 +1,31 @@
 window.addEventListener('DOMContentLoaded', () => {
     console.log("load")
-    displayNumber()
+    displayNumber();
+    // calculate();
 })
 
 function displayNumber() {
     const display = document.getElementById('display');
     const numberGrid = document.getElementById('number-grid');
+    let calculation = 0
 
     numberGrid.addEventListener('click', (e) => {
-        console.log(e.target.innerText)
+        // console.log(e.target.innerText)
         if (display.innerText === '0') {
-            display.innerText = e.target.innerText
+            display.innerText = e.target.innerText;
+            calculation = e.target.innerText;
+        } else if (e.target.innerText === "=") {
+            calculate(calculation)
         } else {
             display.innerText += e.target.innerText
+            calculation += e.target.innerText;
         }
+
     })
     
+}
+
+function calculate(calculation) {
+    console.log(parseInt(calculation))
+    calculation.split()
 }
